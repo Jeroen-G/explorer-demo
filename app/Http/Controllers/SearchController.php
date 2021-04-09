de995c2d4230bdeaeeeb969c936d2564c2daa1e2
Jeroen <jgroenendijk@enrise.com>
2021-04-09 16:29:06 +0200

    add example of custom analysis

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\SearchFormRequest;
 use App\Models\Cartographer;
-use JeroenG\Explorer\Domain\Syntax\Matching;
 use JeroenG\Explorer\Domain\Syntax\Terms;
 use JeroenG\Explorer\Infrastructure\Scout\ElasticEngine;
 
@@ -28,7 +27,7 @@ class SearchController extends Controller
             $search->must(new Terms('place', $request->get('countries')));
         }
 
-        $people = $search->get();
+        $people = $search->take(100)->get();
 
         return view('search', [
             'people' => $people,
--- app/Models/Cartographer.php
+++ app/Models/Cartographer.php
@@ -5,9 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use JeroenG\Explorer\Application\Explored;
+use JeroenG\Explorer\Application\IndexSettings;
 use Laravel\Scout\Searchable;
 
-class Cartographer extends Model implements Explored
+class Cartographer extends Model implements Explored, IndexSettings
 {
     use HasFactory;
     use Searchable;
@@ -18,9 +19,35 @@ class Cartographer extends Model implements Explored
     {
         return [
             'id' => 'keyword',
-            'name' => 'text',
+            'name' => [
+                'type' => 'text',
+                'analyzer' => 'synonym',
+            ],
             'place' => 'keyword',
             'lifespan' => 'text',
+        ];
+    }
+
+    public function indexSettings(): array
+    {
+        return [
+            'analysis' => [
+                'analyzer' => [
+                    'synonym' => [
+                        'tokenizer' => 'standard',
+                        'filter' => ['lowercase', 'synonym'],
+                    ],
+                ],
+                'filter' => [
+                    'synonym' => [
+                        'type' => 'synonym',
+                        'lenient' => true,
+                        'synonyms' => [
+                            'johannes => johan',
+                        ],
+                    ],
+                ],
+            ],
         ];
     }
 }
